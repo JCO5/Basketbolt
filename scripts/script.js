ca88181daea7175a22de3437bd05e2f4a623f4b1
JCO5 <joaquinopulencia99@gmail.com>
2023-07-04 14:46:21 -0700

    major html revamp including modal, game screens, buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
 "use strict";
 
+// Creating the canvas element in JS so that my autocomplete works in vs code
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+document.getElementById("game-screen").appendChild(canvas);
+canvas.width = 1200;
+canvas.height = 600;
