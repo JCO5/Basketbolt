db9f420555870d28451b3635233f4c9f9de1778a
JCO5 <joaquinopulencia99@gmail.com>
2023-07-05 10:47:38 -0700

    radio player select

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,28 @@
 "use strict";
 
-// Creating the canvas element in JS so that my autocomplete works in vs code
 const canvas = document.createElement("canvas");
 canvas.id = "canvas";
 document.getElementById("game-screen").appendChild(canvas);
 canvas.width = 1200;
 canvas.height = 600;
+
+const game = {
+    isRunning: false,
+    currentScreen: null,
+    // DOM variables
+    elems: {
+        startScreen: {
+            playerNameInput: document.getElementById("name-input"),
+            playBtn: document.getElementById("play-btn"),
+            rulesBtn: document.getElementById("rules-btn"),
+            playerSelect: document.getElementById("player-select"),
+        },
+        gameScreen: {
+            
+        },
+        gameOverScreen: {
+          
+        },
+
+        
+}}
